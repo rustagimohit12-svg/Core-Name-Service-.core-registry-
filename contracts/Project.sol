4e838364a5b75c7555b062ccb8fd6d455099d4db
rustagimohit12-svg <rustagimohit12@gmail.com>
2025-10-25 16:45:28 +0530

    Update Project.sol
    Wrote a small contract showing the project name and description

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-pragma solidity >=0.8.2 <0.9.0;
+contract Identity {
+    string Project;
+    string Description;
 
-contract Core-Name-Service-.core-registry {
+    constructor() {
+        Project = "Core-Name-Service-.core-registry-";
+        Description = unicode"Core Name Service (.core Registry) is a decentralized naming system on the Core Blockchain that maps human-readable names (like user.core) to blockchain addresses and resources. It simplifies transactions, enhances identity management, and improves accessibility within the Core ecosystem.";
+    }
+    
+    function showProject() public view returns (string memory) {
+        return Project;
+    }
+    
+    function showDescription() public view returns (string memory) {
+        return Description; 
+    }
 }
